9988f6e93c439fb912a7183a30c044da98d9bb9a
keyanskv <81758062+keyanskv@users.noreply.github.com>
2023-09-23 10:08:25 +0000

    Update challenge_2.py

--- Unit 1 - Challenge/challenge_2.py
+++ Unit 1 - Challenge/challenge_2.py
@@ -19,6 +19,6 @@ year = int(input("Enter a year: "))
 
 # Check if it's a leap year and print the result
 if is_leap_year(year):
-    print(f"{year} is a leap year.")
+    print(year," is a leap year.")
 else:
-    print(f"{year} is not a leap year.")
+    print(year," is not a leap year.")
